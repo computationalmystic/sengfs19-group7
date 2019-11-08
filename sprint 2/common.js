443dee3700c794beced402ac559945a6b3ec9512
esb7cp <54416561+esb7cp@users.noreply.github.com>
2019-11-08 11:44:38 -0600

    Add files via upload

--- sprint 2/common.js
+++ sprint 2/common.js
@@ -12,22 +12,22 @@ function getNavBar()
 }
 function displayNavBar(x)
 {
-    document.getElementById("navbar").innerHTML = "<a id='home' href = 'http://augur.osshealth.io/'>Home</a><a id='data1' href = 'contributers.html'>Contributers</a><a id='data2'>Issues</a><a id='data1'>Commits</a>";
+    document.getElementById("navbar").innerHTML = "<a id='home' href = 'http://augur.osshealth.io/'>Home</a><a id='data1' href = 'contributers.html'>Contributers</a><a id='data2' href = 'issues.html'>Issues</a><a id='data3' href = 'commits.html'>Commits</a> <a href='javascript:void(0);' class='click' onclick='getNavBar()'> <i class='fa fa-bars'></i> </a>";
     if (x == 0)
     {
-        document.getElementById("home").className = "active";
+        document.getElementById("home").className = "activeButton";
     }
     if (x == 1)
     {
-        document.getElementById("data1").classname = "active";
+        document.getElementById("data1").className = "activeButton";
     }
     if (x == 2)
     {
-        document.getElementById("data2").classname = "active";
+        document.getElementById("data2").className = "activeButton";
     }
     if (x == 3)
     {
-        document.getElementById("data3").classname = "active";
+        document.getElementById("data3").className = "activeButton";
     }
 }
 
