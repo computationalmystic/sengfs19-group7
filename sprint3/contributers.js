c4dc1bac0bf01b0163ca128568d5a24f54f716df
esb7cp <esb7cp@mail.missouri.edu>
2019-11-19 17:12:28 -0600

    got chart to populate and dynamically alter chart title

--- sprint3/contributers.js
+++ sprint3/contributers.js
@@ -1,4 +1,43 @@
-function repos()
-{
-    
+//http://augur.osshealth.io/api_docs/
+
+function loadGraph() {
+     
+    var repoId = document.getElementById("repoId").value ; 
+    var groupId = document.getElementById("groupId").value ;
+     
+
+    var dataPoints = [];
+
+    var chart1 = new CanvasJS.Chart("chartContainer1",{
+        title:{
+            text:"Representation of contributers for "
+        },
+        axisX:{
+
+            title: "Contributer number"
+        },
+        axisY:{
+
+            title: "total contributions (commits + issues + comments)"
+
+        },
+            data: [{
+            type: "column",
+            dataPoints : dataPoints
+
+
+        }]
+    });
+        var obj = $.getJSON("http://augur.osshealth.io:5000/api/unstable/repo-groups/" + groupId + "/repos/" + repoId + "/contributors", function(data) {
+            var x = JSON.parse(obj.responseText);
+            console.log(x);
+            $.each(data, function(key, value){
+                chart1.options.title.text = "Representation of contributers for  " + value.repo_name;
+                dataPoints.push({x: parseInt(value.user_id), y: parseInt(value.total)});
+                console.log(value);
+            });
+            chart1.render();
+
+        });
+
 }
