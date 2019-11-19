9a741ec187c491c438322786b9f30aade434aa58
njewwc <54416519+njewwc@users.noreply.github.com>
2019-11-19 14:52:40 -0600

    Add files via upload

--- sprint3/commits.js
+++ sprint3/commits.js
@@ -3,12 +3,21 @@
     let repoId = document.getElementById("repoId").value ; 
     let groupId = document.getElementById("groupId").value ;
      
-     
+
 var dataPoints = [];
 
 var chart = new CanvasJS.Chart("chartContainer",{
     title:{
         text:"Representation of top commiters"
+    },
+    axisX:{
+        
+        title: "emails of commiters"
+    },
+    axisY:{
+        
+        title: "total commits"
+        
     },
         data: [{
         type: "column",
@@ -17,9 +26,11 @@ var chart = new CanvasJS.Chart("chartContainer",{
         
     }]
 });
-$.getJSON("http://augur.osshealth.io:5000/api/unstable/repo-groups/24/repos/21623/top-committers", function(data) {  
+    $.getJSON("http://augur.osshealth.io:5000/api/unstable/repo-groups/24/repos/21623/top-committers", function(data) {  
     $.each(data, function(key, value){
-        dataPoints.push({x: key[0], y: parseInt(value[1])});
+         console.log(value.email);
+        dataPoints.push({label: value.email, y: parseInt(value.commits)});
+        console.log(value);
     });
     chart.render();
   
