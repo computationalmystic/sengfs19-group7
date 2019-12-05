bc28ea7e1dccbc32f61a3bef1c266192e3df59aa
esb7cp <esb7cp@mail.missouri.edu>
2019-12-05 13:58:32 -0600

    Update contributers.js

--- sprint3/contributers.js
+++ sprint3/contributers.js
@@ -5,7 +5,16 @@ function loadGraph() {
     var repoId = document.getElementById("repoId").value ; 
     var groupId = document.getElementById("groupId").value ;
      
-
+     $.ajax({
+    url: "http://augur.osshealth.io:5000/api/unstable/repo-groups/"+ groupId +"/repos/"+repoId+"/contributors",
+    statusCode: {
+        500: function() {
+            alert(" 500 Error Was returned for the Contributors API Endpoint");
+            console.log('500 ');
+        }
+    }
+    }); 
+    
     var dataPoints = [];
 
     var chart1 = new CanvasJS.Chart("chartContainer1",{
@@ -32,9 +41,15 @@ function loadGraph() {
             $.each(data, function(key, value){
                 chart1.options.title.text = "Representation of contributers for " + value.repo_name;
                 dataPoints.push({label: parseInt(value.user_id), y: parseInt(value.total)});
-                //console.log(value);
+                console.log(value);
+                if (value == null){
+                    alert("Error. Data is NULL");
+                    console.log("null");
+                    break;
+                }
             });
             chart1.render();
+         
 
         });
 }
@@ -44,7 +59,17 @@ function loadGraph2() {
     var repoId = document.getElementById("repoId").value ; 
     var groupId = document.getElementById("groupId").value ;
      
-
+         $.ajax({
+    url: "http://augur.osshealth.io:5000/api/unstable/repo-groups/"+ groupId +"/repos/"+repoId+"/contributors-new",
+    statusCode: {
+        500: function() {
+            alert(" 500 Error Was returned for the Contributors-New API Endpoint");
+            console.log('500 ');
+        }
+    }
+    }); 
+    
+    
     var dataPoints = [];
 
     var chart1 = new CanvasJS.Chart("chartContainer2",{
@@ -85,6 +110,15 @@ function loadGraph3() {
     var repoId = document.getElementById("repoId").value ; 
     var groupId = document.getElementById("groupId").value ;
      
+             $.ajax({
+    url: "http://augur.osshealth.io:5000/api/unstable/repo-groups/"+ groupId +"/repos/"+repoId+"/pull-requests-merge-contributor-new",
+    statusCode: {
+        500: function() {
+            alert(" 500 Error Was returned for the New Contributors of Commits API Endpoint");
+            console.log('500 ');
+        }
+    }
+    }); 
 
     var dataPoints = [];
 
